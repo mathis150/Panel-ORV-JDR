7b1e45b0632eb6406e43df25bfa12dff68f74078
mathis150 <48561648+mathis150@users.noreply.github.com>
2025-07-18 23:57:58 +0200

    🎉 Begin of the project !

---
+++ index.php
@@ -0,0 +1,16 @@
+<html lang="fr">
+    <head>
+        <meta charset="utf-8">
+        <title>Connexion - ORV JDR</title>
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Koh+Santepheap:wght@100;300;400;700;900&display=swap" rel="stylesheet">
+        <link href='http://fonts.googleapis.com/css?family=Roboto:400,100,100italic,300,300italic,400italic,500,500italic,700,700italic,900italic,900' rel='stylesheet' type='text/css'>
+        <link href="./css/style.css" rel="stylesheet">
+    </head>
+    <body>
+        <div style="font-family: 'Roboto';">UwU</div>
+        <div style="font-family: 'Koh Santepheap';">UwU</div>
+        <div>UwU</div>
+    </body>
+</html>
