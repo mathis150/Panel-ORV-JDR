c9a0f1e309dcfe54a6a7fdd87bbd3336c1f0e023
mathis150 <48561648+mathis150@users.noreply.github.com>
2025-07-19 11:22:35 +0200

    💄Create background style

--- index.php
+++ index.php
@@ -7,10 +7,21 @@
         <link href="https://fonts.googleapis.com/css2?family=Koh+Santepheap:wght@100;300;400;700;900&display=swap" rel="stylesheet">
         <link href='http://fonts.googleapis.com/css?family=Roboto:400,100,100italic,300,300italic,400italic,500,500italic,700,700italic,900italic,900' rel='stylesheet' type='text/css'>
         <link href="./css/style.css" rel="stylesheet">
+
+        <style>
+            /* Nommation : <type>_<subtype>-<variation> */
+
+            /*1. Bases*/
+            body {
+                margin: 0;
+                padding : 0;
+
+                font-family: 'Roboto', sans-serif;
+
+                background: linear-gradient(107deg, #26314F 1.63%, #2B3758 17.13%, #2D3A5D 32.22%, #313F65 50.56%, #384874 78.71%, #44588E 95.17%, #5770B5 100.35%);
+            }
+        </style>
     </head>
     <body>
-        <div style="font-family: 'Roboto';">UwU</div>
-        <div style="font-family: 'Koh Santepheap';">UwU</div>
-        <div>UwU</div>
     </body>
 </html>
