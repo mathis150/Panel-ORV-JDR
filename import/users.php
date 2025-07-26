871d8403312b0596a1416d257d6453de9656bfa9
mathis150 <48561648+mathis150@users.noreply.github.com>
2025-07-26 17:09:50 +0200

    🔨 Usefuls functions for managing users

--- import/users.php
+++ import/users.php
@@ -9,6 +9,33 @@
         public function __construct() {
             $this->database = new Database();
         }
+        public function checkIfUserExist($pseudo = "", $email = "") {
+            if(isset($pseudo)) {
+                $returned = $this->database->makeSQLRequest("SELECT * FROM users WHERE pseudonyme = ?", array($this->filtrerTexteSQL($pseudo)), ORV_SQL_COUNT);
+
+                return ($returned == 1) ? true : false;
+            } elseif(isset($pseudo)) {
+                $returned = $this->database->makeSQLRequest("SELECT * FROM users WHERE email = ?", array($this->filtrerTexteSQL($email)), ORV_SQL_COUNT);
+
+                return ($returned == 1) ? true : false;
+            } else {
+                return "Need arguments.";
+            }
+        }
+
+        public function isPasswordValid($password, $password_hash) {
+            return password_verify($password, $password_hash);
+        }
+
+        public function hashPasswordTo($password) {
+            return password_hash($password, $this->hash);
+        }
+
+        public function changeUserPassword($uuid, $newPassword) {
+            $passwordHash = $this->hashPasswordTo($newPassword);
+
+            $this->database->makeSQLRequest("UPDATE users SET password=? WHERE uuid = ?", array($passwordHash, $uuid));
+        }
         public function getUserDataBySession($token) {}
 
         public function editUserByUUID($uuid, $pseudo = "", $email = "", $password = "") {}
