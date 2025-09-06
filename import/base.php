ca89372c3dd9fed7fd23afba92a8641bb00f92d3
mathis150 <48561648+mathis150@users.noreply.github.com>
2025-09-07 00:30:22 +0200

    🔨 Class for base of the website

---
+++ import/base.php
@@ -0,0 +1,89 @@
+<?php
+    class WebPageBases {
+
+        private $pagesNames = array (
+            "dashboard" => "Dashboard"
+        );
+        private $pagesList = array(
+            0 => array(
+                "name" => "Dashboard",
+                "active" => "dashboard",
+                "permissions" => ""
+            ),
+            1 => array(
+                "name" => "Vos fiches",
+                "active" => "files",
+                "permissions" => ""
+            ),
+            2 => array(
+                "name" => "Gestion des sessions",
+                "active" => "sessions",
+                "permissions" => "admin"
+            ),
+            3 => array(
+                "name" => "Combats",
+                "active" => "fights",
+                "permissions" => "admin"
+            ),
+            4 => array(
+                "name" => "Parametres",
+                "active" => "jdr-params",
+                "permissions" => "admin"
+            )
+        );
+
+        public function __construct() { }
+
+        public function __destruct() { }
+
+        public function header() {
+            $page = $this->pagesNames[$this->getPage()];
+            ?>
+            <html lang="fr">
+                <head>
+                    <meta charset="utf-8">
+                    <title><?php echo $page; ?> - ORV JDR</title>
+                    <link rel="preconnect" href="https://fonts.googleapis.com">
+                    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+                    <link href="https://fonts.googleapis.com/css2?family=Koh+Santepheap:wght@100;300;400;700;900&display=swap" rel="stylesheet">
+                    <link href='http://fonts.googleapis.com/css?family=Roboto:400,100,100italic,300,300italic,400italic,500,500italic,700,700italic,900italic,900' rel='stylesheet' type='text/css'>
+                    <link href="./css/style.css" rel="stylesheet">
+                </head>
+            <?php
+        }
+
+        public function navigation() {
+            $page = $this->getPage();
+            ?>
+            <nav>
+                <section  class="navigation_info">
+                    <a class="nav-button" href="./"><img src="./img/generic/LogoOrv.png" width="60px"></a>
+                    <a class="nav-button nav-title" href="./">Panneau de gestion<br>du JDR ORV</a>
+                </section>
+                <section class="navigation_container">
+                    <?php
+                        foreach($this->pagesList as $key => $values)
+                        {
+                            echo '<a class="nav-button ';
+                            if($values["active"] == $page) {
+                                echo "nav-active";
+                            }
+                            echo '" href="./'.$values['active'].'">'.$values['name'].'</a>';
+                        }
+                    ?>
+                </section>
+                <section class="navigation_info">
+                    <a href="#" class="nav-profil"><img src="./img/generic/LogoOrv.png" width="60px"> <div class="nav-profil_text">Administrateur</div></a>
+                </section>
+            </nav>
+            <?php
+        }
+
+        private function getPage() {
+            $page = explode("/", explode("?", $_SERVER['REQUEST_URI'])[0]);
+
+            return $page[count($page) - 1];
+        }
+    }
+
+?>
