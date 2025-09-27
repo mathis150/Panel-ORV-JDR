1092e35d5f9b03411b5bb87115424aff594f56b2
mathis150 <48561648+mathis150@users.noreply.github.com>
2025-09-27 23:41:58 +0200

    💄Setup every forms for users management

---
+++ sub-pages/users.create.php
@@ -0,0 +1,35 @@
+<div class="orv-buttons" style="justify-content: flex-end;">
+    <button class="orv-edit"><i class="fa-solid fa-check"></i> <span style="font-size: 18px;">Sauvegarder</span></button>
+    <button class="orv-edit"><i class="fa-solid fa-xmark"></i> <span style="font-size: 18px;">Annuler</span></button>
+</div>
+<div class="orv-menu" id="gest-perso">
+    <div class="orv-menu_header">&lt;Création d'un utilisateur&gt;</div>
+    <div class="orv-menu_container">
+        <div class="orv-menu_form">
+            <div>
+                <h2 class="classic-title">Informations générales :</h2>
+                <cite>Toutes les informations importantes pour le compte. (Le mot de passe sera envoyé automatiquement à l'utilisateur, avec son e-mail.)</cite>
+            </div>
+            <div class="orv-menu_form-elements">
+                <div class="orv-menu_form-inputs-list">
+                    <div class="orv-menu_form-input">
+                        <label>Pseudonyme :</label>
+                        <input type="number">
+                    </div>
+                    <div class="orv-menu_form-input">
+                        <label>Adresse E-Mail :</label>
+                        <input type="number">
+                    </div>
+                    <div class="orv-menu_form-input">
+                        <label>Type d'utilisateur:</label>
+                        <select>
+                            <option value="sudo">Super-Administrateur</option>
+                            <option value="admin">Administrateur</option>
+                            <option value="player" selected>Joueur</option>
+                        </select>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
