6f098729c147af24f194ea1e7fa7521ecbf8f9ab
mathis150 <48561648+mathis150@users.noreply.github.com>
2025-09-27 23:17:51 +0200

    💄 Design of every pages of the jdr params

---
+++ sub-pages/characters.list.php
@@ -0,0 +1,48 @@
+
+<div class="orv-buttons" style="justify-content: flex-end;">
+    <button class="orv-edit"><i class="fa-solid fa-plus"></i> <span style="font-size: 18px;">Créer un utilisateur</span></button>
+</div>
+<div class="orv-menu" id="edit-perso">
+    <div class="orv-menu_header">&lt;Liste des Caractères&gt;</div>
+    <div class="orv-menu_container" style="padding: 0;">
+        <table>
+            <thead>
+                <tr>
+                    <td>Nom</td>
+                    <td>Sponsor</td>
+                    <td>Stats</td>
+                    <td width="80px">Actions</td>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>eeeeee</td>
+                    <td>eeeee</td>
+                    <td>ee</td>
+                    <td>
+                        <a href="#" style="background: #FFF; font-size: 24px; background-clip: text !important; -webkit-background-clip: text !important;"><i class="fa-solid fa-pen"></i></a> 
+                        <a href="#" style="background: #FFF; font-size: 24px; background-clip: text !important; -webkit-background-clip: text !important;"><i class="fa-solid fa-trash"></i></a>
+                    </td>
+                </tr>
+                <tr>
+                    <td>eeeeeeeee</td>
+                    <td>eeeeee</td>
+                    <td>ee</td>
+                    <td>
+                        <a href="#" style="background: #FFF; font-size: 24px; background-clip: text !important; -webkit-background-clip: text !important;"><i class="fa-solid fa-pen"></i></a> 
+                        <a href="#" style="background: #FFF; font-size: 24px; background-clip: text !important; -webkit-background-clip: text !important;"><i class="fa-solid fa-trash"></i></a>
+                    </td>
+                </tr>
+                <tr>
+                    <td>eeeeeeeee</td>
+                    <td>eeeeeeeee</td>
+                    <td>ee</td>
+                    <td>
+                        <a href="#" style="background: #FFF; font-size: 24px; background-clip: text !important; -webkit-background-clip: text !important;"><i class="fa-solid fa-pen"></i></a> 
+                        <a href="#" style="background: #FFF; font-size: 24px; background-clip: text !important; -webkit-background-clip: text !important;"><i class="fa-solid fa-trash"></i></a>
+                    </td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+</div>
