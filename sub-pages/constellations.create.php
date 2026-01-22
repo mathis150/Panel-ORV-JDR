68a0b467d66a4746e233382f8799aa5697c25174
mathis150 <48561648+mathis150@users.noreply.github.com>
2026-01-22 23:03:19 +0100

    💄 Update Constellations Creation Form

--- sub-pages/constellations.create.php
+++ sub-pages/constellations.create.php
@@ -26,7 +26,20 @@
                     </div>
                     <div class="orv-menu_form-input">
                         <label>Rang :</label>
-                        <select>
+                        <select style="width: 350px;">
+                            <option value="myth">Mythique</option>
+                            <option value="legend">Légendaire</option>
+                            <option value="historic">Historique</option>
+                            <option value="narrative" selected>Narrative</option>
+                        </select>
+                    </div>
+                    <div class="orv-menu_form-input">
+                        <label>Race de la constellation :</label>
+                        <input type="text">
+                    </div>
+                    <div class="orv-menu_form-input">
+                        <label>Nébuleuse de la constellation :</label>
+                        <select style="width: 350px;">
                             <option value="myth">Mythique</option>
                             <option value="legend">Légendaire</option>
                             <option value="historic">Historique</option>
@@ -36,5 +49,130 @@
                 </div>
             </div>
         </div>
+        <hr>
+        <div class="orv-menu_form">
+            <div>
+                <h2 class="classic-title">Psychologie de la constellation :</h2>
+                <cite>Comment est votre constellation ?</cite>
+            </div>
+            <div class="orv-menu_form-elements">
+                <div class="orv-menu_form-inputs-list">
+                    <div class="orv-menu_form-input" style="width: 325px;">
+                        <label>Psyché de la constellation :</label>
+                        <textarea></textarea>
+                    </div>
+                    <div class="orv-menu_form-input" style="width: 325px;">
+                        <label>Vertu de la constellation :</label>
+                        <textarea></textarea>
+                    </div>
+                    <div class="orv-menu_form-input" style="width: 325px;">
+                        <label>Vice de la constellation :</label>
+                        <textarea></textarea>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <hr>
+        <div class="orv-menu_form">
+            <div>
+                <h2 class="classic-title">Informations relatifs au JDR :</h2>
+                <cite>Comment est votre personnage ?</cite>
+            </div>
+            <div class="orv-menu_form-elements">
+                <div class="orv-menu_form-inputs-list">
+                    <div class="orv-menu_form-input">
+                        <label>Force vital :</label>
+                        <input type="number">
+                    </div>
+                    <div class="orv-menu_form-input">
+                        <label>Force physique :</label>
+                        <input type="number">
+                    </div>
+                    <div class="orv-menu_form-input">
+                        <label>Agilité :</label>
+                        <input type="number">
+                    </div>
+                    <div class="orv-menu_form-input">
+                        <label>Puissance magique :</label>
+                        <input type="number">
+                    </div>
+                    <div class="orv-menu_form-input">
+                        <label>Argent en sa possession :</label>
+                        <input type="number">
+                    </div>
+                </div>
+            </div>
+            <div class="orv-menu_form-elements">
+                <div class="orv-menu_form-inputs-list">
+                    <div class="orv-menu_form-input" style="width: 325px;">
+                        <label>Charactéristiques spéciales :</label>
+                        <select id="example-multi-select2" data-placeholder="Select options" multiple="multiple">
+                            <option value="option1">Option 1</option>
+                            <option value="option2">Option 2</option>
+                        </select>
+                    </div>
+                    <div class="orv-menu_form-input" style="width: 325px;">
+                        <label>Compétences :</label>
+                        <select id="example-multi-select3" data-placeholder="Select options" multiple="multiple">
+                            <option value="option1">Option 1</option>
+                            <option value="option2">Option 2</option>
+                        </select>
+                    </div>
+                    <div class="orv-menu_form-input" style="width: 325px;">
+                        <label>Stigmates :</label>
+                        <select id="example-multi-select4" data-placeholder="Select options" multiple="multiple">
+                            <option value="option1">Option 1</option>
+                            <option value="option2">Option 2</option>
+                        </select>
+                    </div>
+                    <div class="orv-menu_form-input" style="width: 325px;">
+                        <label>Histoires :</label>
+                        <select id="example-multi-select5" data-placeholder="Select options" multiple="multiple">
+                            <option value="option1">Option 1</option>
+                            <option value="option2">Option 2</option>
+                        </select>
+                    </div>
+                    <div class="orv-menu_form-input" style="width: 325px;">
+                        <label>Titres :</label>
+                        <select id="example-multi-select6" data-placeholder="Select options" multiple="multiple">
+                            <option value="option1">Option 1</option>
+                            <option value="option2">Option 2</option>
+                        </select>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <hr>
+        <div class="orv-menu_form">
+            <div>
+                <h2 class="classic-title">Histoire :</h2>
+                <cite>Qu'a-t-il vécu ?</cite>
+            </div>
+            <div class="orv-menu_form-elements">
+                <div class="orv-menu_form-inputs-list">
+                    <textarea style="width: calc(100% - 20px); height: 200px;"></textarea>
+                </div>
+            </div>
+        </div>
+        <hr>
+        <div class="orv-menu_form">
+            <div>
+                <h2 class="classic-title">Estimations générales :</h2>
+                <cite>Qu'a-t-il vécu ?</cite>
+            </div>
+            <div class="orv-menu_form-elements">
+                <div class="orv-menu_form-inputs-list">
+                    <textarea style="width: calc(100% - 20px); height: 200px;"></textarea>
+                </div>
+            </div>
+        </div>
+        </div>
     </div>
 </div>
+<script>
+    new MultiSelect(document.getElementById('example-multi-select2'));
+    new MultiSelect(document.getElementById('example-multi-select3'));
+    new MultiSelect(document.getElementById('example-multi-select4'));
+    new MultiSelect(document.getElementById('example-multi-select5'));
+    new MultiSelect(document.getElementById('example-multi-select6'));
+</script>
