836200b1c763c3a241e723f1c1ca9dcd02c51d61
mathis150 <48561648+mathis150@users.noreply.github.com>
2026-01-22 23:37:37 +0100

    💄Creation Constellations Edition page

---
+++ sub-pages/constellations.modify.php
@@ -0,0 +1,408 @@
+<div class="orv-buttons" style="justify-content: flex-end;">
+    <button class="orv-edit"><i class="fa-solid fa-check"></i> <span style="font-size: 18px;">Sauvegarder</span></button>
+    <button class="orv-edit"><i class="fa-solid fa-xmark"></i> <span style="font-size: 18px;">Annuler</span></button>
+</div>
+<div class="orv-menu" id="gest-perso">
+    <div class="orv-menu_header">&lt;Modification d'une constellation&gt;</div>
+    <div class="orv-menu_container">
+        <div class="orv-menu_form">
+            <div>
+                <h2 class="classic-title">Informations générales :</h2>
+                <cite>Toutes les informations importantes à propos de la constellation.</cite>
+            </div>
+            <div class="orv-menu_form-elements">
+                <div class="orv-menu_form-inputs-list">
+                    <div class="orv-menu_form-input">
+                        <label>Modificateur principal :</label>
+                        <input type="number">
+                    </div>
+                    <div class="orv-menu_form-input">
+                        <label>Identité :</label>
+                        <input type="number">
+                    </div>
+                    <div class="orv-menu_form-input">
+                        <label>Provenance :</label>
+                        <input type="number">
+                    </div>
+                    <div class="orv-menu_form-input">
+                        <label>Rang :</label>
+                        <select style="width: 350px;">
+                            <option value="myth">Mythique</option>
+                            <option value="legend">Légendaire</option>
+                            <option value="historic">Historique</option>
+                            <option value="narrative" selected>Narrative</option>
+                        </select>
+                    </div>
+                    <div class="orv-menu_form-input">
+                        <label>Race de la constellation :</label>
+                        <input type="text">
+                    </div>
+                    <div class="orv-menu_form-input">
+                        <label>Nébuleuse de la constellation :</label>
+                        <select style="width: 350px;">
+                            <option value="myth">Mythique</option>
+                            <option value="legend">Légendaire</option>
+                            <option value="historic">Historique</option>
+                            <option value="narrative" selected>Narrative</option>
+                        </select>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <hr>
+        <div class="orv-menu_form">
+            <div>
+                <h2 class="classic-title">Psychologie de la constellation :</h2>
+                <cite>Comment est votre constellation ?</cite>
+            </div>
+            <div class="orv-menu_form-elements">
+                <div class="orv-menu_form-inputs-list">
+                    <div class="orv-menu_form-input" style="width: 325px;">
+                        <label>Psyché de la constellation :</label>
+                        <textarea></textarea>
+                    </div>
+                    <div class="orv-menu_form-input" style="width: 325px;">
+                        <label>Vertu de la constellation :</label>
+                        <textarea></textarea>
+                    </div>
+                    <div class="orv-menu_form-input" style="width: 325px;">
+                        <label>Vice de la constellation :</label>
+                        <textarea></textarea>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <hr>
+        <div class="orv-menu_form">
+            <div>
+                <h2 class="classic-title">Informations relatifs au JDR :</h2>
+                <cite>Comment est votre personnage ?</cite>
+            </div>
+            <div class="orv-menu_form-elements">
+                <div class="orv-menu_form-inputs-list">
+                    <div class="orv-menu_form-input">
+                        <label>Force vital :</label>
+                        <input type="number">
+                    </div>
+                    <div class="orv-menu_form-input">
+                        <label>Force physique :</label>
+                        <input type="number">
+                    </div>
+                    <div class="orv-menu_form-input">
+                        <label>Agilité :</label>
+                        <input type="number">
+                    </div>
+                    <div class="orv-menu_form-input">
+                        <label>Puissance magique :</label>
+                        <input type="number">
+                    </div>
+                    <div class="orv-menu_form-input">
+                        <label>Argent en sa possession :</label>
+                        <input type="number">
+                    </div>
+                </div>
+            </div>
+        </div>
+        <hr>
+        <div class="orv-menu_form">
+            <div>
+                <h2 class="classic-title">Histoire :</h2>
+                <cite>Qu'a-t-il vécu ?</cite>
+            </div>
+            <div class="orv-menu_form-elements">
+                <div class="orv-menu_form-inputs-list">
+                    <textarea style="width: calc(100% - 20px); height: 200px;"></textarea>
+                </div>
+            </div>
+        </div>
+        <hr>
+        <div class="orv-menu_form">
+            <div>
+                <h2 class="classic-title">Estimations générales :</h2>
+                <cite>Qu'a-t-il vécu ?</cite>
+            </div>
+            <div class="orv-menu_form-elements">
+                <div class="orv-menu_form-inputs-list">
+                    <textarea style="width: calc(100% - 20px); height: 200px;"></textarea>
+                </div>
+            </div>
+        </div>
+        <hr>
+        <div class="orv-menu_form">
+            <div>
+                <h2 class="classic-title">Attributs personnels :</h2>
+                <cite>Comment est votre personnage ? (Attention, sauvegardez ce que vous avez fait avant ! Chaque action ci-dessous ce sauvegarde manuellement un par un.)</cite>
+            </div>
+            <div class="orv-menu_form-elements">
+                <div class="orv-menu_form-inputs-list">
+                    <div class="orv-menu_form-input">
+                        <select>
+                            <option value="option1">Option 1</option>
+                            <option value="option2">Option 2</option>
+                        </select>
+                    </div>
+                </div>
+                <div class="orv-menu_form-valid"><button>Ajouter</button></div>
+            </div>
+        </div>
+        <table style="margin-top:10px;">
+            <thead>
+                <tr>
+                    <td>Nom</td>
+                    <td>Niveau</td>
+                    <td>type</td>
+                    <td width="150px">Actions</td>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>eeeeee</td>
+                    <td><input type="number"></td>
+                    <td>ee</td>
+                    <td>
+                        <button>Mettre à jour</button>
+                    </td>
+                </tr>
+                <tr>
+                    <td>eeeeee</td>
+                    <td><input type="number"></td>
+                    <td>ee</td>
+                    <td>
+                        <button>Mettre à jour</button>
+                    </td>
+                </tr>
+                <tr>
+                    <td>eeeeee</td>
+                    <td><input type="number"></td>
+                    <td>ee</td>
+                    <td>
+                        <button>Mettre à jour</button>
+                    </td>
+                </tr>
+            </tbody>
+        </table>
+        <hr>
+        <div class="orv-menu_form">
+            <div>
+                <h2 class="classic-title">Compétences personnels :</h2>
+                <cite>Comment est votre personnage ? (Attention, sauvegardez ce que vous avez fait avant ! Chaque action ci-dessous ce sauvegarde manuellement un par un.)</cite>
+            </div>
+            <div class="orv-menu_form-elements">
+                <div class="orv-menu_form-inputs-list">
+                    <div class="orv-menu_form-input">
+                        <select>
+                            <option value="option1">Option 1</option>
+                            <option value="option2">Option 2</option>
+                        </select>
+                    </div>
+                </div>
+                <div class="orv-menu_form-valid"><button>Ajouter</button></div>
+            </div>
+        </div>
+        <table style="margin-top:10px;">
+            <thead>
+                <tr>
+                    <td>Nom</td>
+                    <td>Niveau</td>
+                    <td>type</td>
+                    <td width="150px">Actions</td>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>eeeeee</td>
+                    <td><input type="number"></td>
+                    <td>ee</td>
+                    <td>
+                        <button>Mettre à jour</button>
+                    </td>
+                </tr>
+                <tr>
+                    <td>eeeeee</td>
+                    <td><input type="number"></td>
+                    <td>ee</td>
+                    <td>
+                        <button>Mettre à jour</button>
+                    </td>
+                </tr>
+                <tr>
+                    <td>eeeeee</td>
+                    <td><input type="number"></td>
+                    <td>ee</td>
+                    <td>
+                        <button>Mettre à jour</button>
+                    </td>
+                </tr>
+            </tbody>
+        </table>
+        <hr>
+        <div class="orv-menu_form">
+            <div>
+                <h2 class="classic-title">Stigmates :</h2>
+                <cite>Comment est votre personnage ? (Attention, sauvegardez ce que vous avez fait avant ! Chaque action ci-dessous ce sauvegarde manuellement un par un.)</cite>
+            </div>
+            <div class="orv-menu_form-elements">
+                <div class="orv-menu_form-inputs-list">
+                    <div class="orv-menu_form-input">
+                        <select>
+                            <option value="option1">Option 1</option>
+                            <option value="option2">Option 2</option>
+                        </select>
+                    </div>
+                </div>
+                <div class="orv-menu_form-valid"><button>Ajouter</button></div>
+            </div>
+        </div>
+        <table style="margin-top:10px;">
+            <thead>
+                <tr>
+                    <td>Nom</td>
+                    <td>Niveau</td>
+                    <td>type</td>
+                    <td width="150px">Actions</td>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>eeeeee</td>
+                    <td><input type="number"></td>
+                    <td>ee</td>
+                    <td>
+                        <button>Mettre à jour</button>
+                    </td>
+                </tr>
+                <tr>
+                    <td>eeeeee</td>
+                    <td><input type="number"></td>
+                    <td>ee</td>
+                    <td>
+                        <button>Mettre à jour</button>
+                    </td>
+                </tr>
+                <tr>
+                    <td>eeeeee</td>
+                    <td><input type="number"></td>
+                    <td>ee</td>
+                    <td>
+                        <button>Mettre à jour</button>
+                    </td>
+                </tr>
+            </tbody>
+        </table>
+        <hr>
+        <div class="orv-menu_form">
+            <div>
+                <h2 class="classic-title">Histoires :</h2>
+                <cite>Comment est votre personnage ? (Attention, sauvegardez ce que vous avez fait avant ! Chaque action ci-dessous ce sauvegarde manuellement un par un.)</cite>
+            </div>
+            <div class="orv-menu_form-elements">
+                <div class="orv-menu_form-inputs-list">
+                    <div class="orv-menu_form-input">
+                        <select>
+                            <option value="option1">Option 1</option>
+                            <option value="option2">Option 2</option>
+                        </select>
+                    </div>
+                </div>
+                <div class="orv-menu_form-valid"><button>Ajouter</button></div>
+            </div>
+        </div>
+        <table style="margin-top:10px;">
+            <thead>
+                <tr>
+                    <td>Nom</td>
+                    <td>Niveau</td>
+                    <td>type</td>
+                    <td width="150px">Actions</td>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>eeeeee</td>
+                    <td><input type="number"></td>
+                    <td>ee</td>
+                    <td>
+                        <button>Mettre à jour</button>
+                    </td>
+                </tr>
+                <tr>
+                    <td>eeeeee</td>
+                    <td><input type="number"></td>
+                    <td>ee</td>
+                    <td>
+                        <button>Mettre à jour</button>
+                    </td>
+                </tr>
+                <tr>
+                    <td>eeeeee</td>
+                    <td><input type="number"></td>
+                    <td>ee</td>
+                    <td>
+                        <button>Mettre à jour</button>
+                    </td>
+                </tr>
+            </tbody>
+        </table>
+        <hr>
+        <div class="orv-menu_form">
+            <div>
+                <h2 class="classic-title">Titres :</h2>
+                <cite>Comment est votre personnage ? (Attention, sauvegardez ce que vous avez fait avant ! Chaque action ci-dessous ce sauvegarde manuellement un par un.)</cite>
+            </div>
+            <div class="orv-menu_form-elements">
+                <div class="orv-menu_form-inputs-list">
+                    <div class="orv-menu_form-input">
+                        <select>
+                            <option value="option1">Option 1</option>
+                            <option value="option2">Option 2</option>
+                        </select>
+                    </div>
+                </div>
+                <div class="orv-menu_form-valid"><button>Ajouter</button></div>
+            </div>
+        </div>
+        <table style="margin-top:10px;">
+            <thead>
+                <tr>
+                    <td>Nom</td>
+                    <td>Niveau</td>
+                    <td>type</td>
+                    <td width="150px">Actions</td>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>eeeeee</td>
+                    <td><input type="number"></td>
+                    <td>ee</td>
+                    <td>
+                        <button>Mettre à jour</button>
+                    </td>
+                </tr>
+                <tr>
+                    <td>eeeeee</td>
+                    <td><input type="number"></td>
+                    <td>ee</td>
+                    <td>
+                        <button>Mettre à jour</button>
+                    </td>
+                </tr>
+                <tr>
+                    <td>eeeeee</td>
+                    <td><input type="number"></td>
+                    <td>ee</td>
+                    <td>
+                        <button>Mettre à jour</button>
+                    </td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+</div>
+<script>
+    new MultiSelect(document.getElementById('example-multi-select2'));
+    new MultiSelect(document.getElementById('example-multi-select3'));
+    new MultiSelect(document.getElementById('example-multi-select4'));
+    new MultiSelect(document.getElementById('example-multi-select5'));
+    new MultiSelect(document.getElementById('example-multi-select6'));
+</script>
