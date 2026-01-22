200a7b76fef02328203aa044b1ee7c3805f1325d
mathis150 <48561648+mathis150@users.noreply.github.com>
2026-01-22 23:36:46 +0100

    💄Update Characters List links

--- sub-pages/characters.list.php
+++ sub-pages/characters.list.php
@@ -1,6 +1,6 @@
 
 <div class="orv-buttons" style="justify-content: flex-end;">
-    <button class="orv-edit"><i class="fa-solid fa-plus"></i> <span style="font-size: 18px;">Créer un utilisateur</span></button>
+    <button class="orv-edit" onclick="window.location.href='jdr-params?page=characters&sub-page=create';"><i class="fa-solid fa-plus"></i> <span style="font-size: 18px;">Créer un utilisateur</span></button>
 </div>
 <div class="orv-menu" id="edit-perso">
     <div class="orv-menu_header">&lt;Liste des Caractères&gt;</div>
@@ -20,8 +20,8 @@
                     <td>eeeee</td>
                     <td>ee</td>
                     <td>
-                        <a href="#" style="background: #FFF; font-size: 24px; background-clip: text !important; -webkit-background-clip: text !important;"><i class="fa-solid fa-pen"></i></a> 
-                        <a href="#" style="background: #FFF; font-size: 24px; background-clip: text !important; -webkit-background-clip: text !important;"><i class="fa-solid fa-trash"></i></a>
+                        <a href="jdr-params?page=characters&sub-page=modify&uuid=" style="background: #FFF; font-size: 24px; background-clip: text !important; -webkit-background-clip: text !important;"><i class="fa-solid fa-pen"></i></a> 
+                        <a href="jdr-params?page=characters&sub-page=delete&uuid=" style="background: #FFF; font-size: 24px; background-clip: text !important; -webkit-background-clip: text !important;"><i class="fa-solid fa-trash"></i></a>
                     </td>
                 </tr>
                 <tr>
@@ -29,8 +29,8 @@
                     <td>eeeeee</td>
                     <td>ee</td>
                     <td>
-                        <a href="#" style="background: #FFF; font-size: 24px; background-clip: text !important; -webkit-background-clip: text !important;"><i class="fa-solid fa-pen"></i></a> 
-                        <a href="#" style="background: #FFF; font-size: 24px; background-clip: text !important; -webkit-background-clip: text !important;"><i class="fa-solid fa-trash"></i></a>
+                        <a href="jdr-params?page=characters&sub-page=modify&uuid=" style="background: #FFF; font-size: 24px; background-clip: text !important; -webkit-background-clip: text !important;"><i class="fa-solid fa-pen"></i></a> 
+                        <a href="jdr-params?page=characters&sub-page=delete&uuid=" style="background: #FFF; font-size: 24px; background-clip: text !important; -webkit-background-clip: text !important;"><i class="fa-solid fa-trash"></i></a>
                     </td>
                 </tr>
                 <tr>
@@ -38,8 +38,8 @@
                     <td>eeeeeeeee</td>
                     <td>ee</td>
                     <td>
-                        <a href="#" style="background: #FFF; font-size: 24px; background-clip: text !important; -webkit-background-clip: text !important;"><i class="fa-solid fa-pen"></i></a> 
-                        <a href="#" style="background: #FFF; font-size: 24px; background-clip: text !important; -webkit-background-clip: text !important;"><i class="fa-solid fa-trash"></i></a>
+                        <a href="jdr-params?page=characters&sub-page=modify&uuid=" style="background: #FFF; font-size: 24px; background-clip: text !important; -webkit-background-clip: text !important;"><i class="fa-solid fa-pen"></i></a> 
+                        <a href="jdr-params?page=characters&sub-page=delete&uuid=" style="background: #FFF; font-size: 24px; background-clip: text !important; -webkit-background-clip: text !important;"><i class="fa-solid fa-trash"></i></a>
                     </td>
                 </tr>
             </tbody>
