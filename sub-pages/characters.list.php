afcdc63786a82c1e2a271e89324a0aa0a9c89449
Mathis LENOIR <48561648+mathis150@users.noreply.github.com>
2026-01-25 14:18:45 +0100

    update

--- sub-pages/characters.list.php
+++ sub-pages/characters.list.php
@@ -1,9 +1,9 @@
 
 <div class="orv-buttons" style="justify-content: flex-end;">
-    <button class="orv-edit" onclick="window.location.href='jdr-params?page=characters&sub-page=create';"><i class="fa-solid fa-plus"></i> <span style="font-size: 18px;">Créer un utilisateur</span></button>
+    <button class="orv-edit" onclick="window.location.href='jdr-params?page=characters&sub-page=create';"><i class="fa-solid fa-plus"></i> <span style="font-size: 18px;">Créer un personnage</span></button>
 </div>
 <div class="orv-menu" id="edit-perso">
-    <div class="orv-menu_header">&lt;Liste des Caractères&gt;</div>
+    <div class="orv-menu_header">&lt;Liste des Personnages&gt;</div>
     <div class="orv-menu_container" style="padding: 0;">
         <table>
             <thead>
